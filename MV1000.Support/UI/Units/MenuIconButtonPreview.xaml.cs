7f207fa14abcc281a230aa73fa29237810bb9278
kyeongrok.kim <kyeongrok.kim@e3view.com>
2026-04-22 21:54:55 +0900

    Preview추가

---
+++ MV1000.Support/UI/Units/MenuIconButtonPreview.xaml.cs
@@ -0,0 +1,16 @@
+using System.Windows.Controls;
+
+namespace MV1000.Support.UI.Units;
+
+public partial class MenuIconButtonPreview : UserControl
+{
+    public MenuIconButtonPreview()
+    {
+        InitializeComponent();
+    }
+
+    private void MenuIconButton_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+
+    }
+}
